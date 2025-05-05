359cdfef88da7d20d3a4fd4fac0f78fab0d54386
YOUNG MIKE <you@example.com>
2025-05-05 16:02:55 +0300

    First commit: Added HTML and images

---
+++ test.js
@@ -0,0 +1,74 @@
+//class MathUtil{
+  //  static PI = 3.14;
+    //static getDiameter(radius){
+      //  return 2 * radius ;
+    //}
+    //static getAreaOfCircle(radius){
+     //   return MathUtil.PI * radius * radius;
+    //}
+//} 
+  //  console.log(MathUtil.PI);   
+   // console.log(MathUtil.getDiameter(10));
+    //console.log(MathUtil.getAreaOfCircle(10));
+
+    //class User{
+      //  static userCount = 0 ;
+        //constructor(username){
+          //  this.username= username ;
+            //User.userCount ++ ;
+        //}
+       // sayHello(){
+       //     console.log("Hey my name is ${this.username}");
+       // }
+            
+        
+    //}
+    //const user1 =new User("Mike Kay");          
+    //const user2 =new User("Marc Cucurella"); 
+
+
+        
+     //user1.sayHello();
+    // user2.sayHello();
+     //console.log(User.userCount);
+
+     //class Rectangle{
+        //constructor(length,width){
+           // this.length = length;
+            //this.width = width;
+
+       // }
+        //get length(){
+         //   return this._length;
+        //}
+        //get width(){
+           // return this._width;
+       // }
+        //set length(newLength){
+            //if(newLength > 0){
+               // this._length = newLength;
+           // }
+           // else{
+              //  console.error("Length must be greater than 0");
+          //  }
+       
+
+        //}
+       // set width(newWidth){
+           // if(newWidth > 0){
+              //  this._width = newWidth;
+           // }
+           //else{
+              //  console.error("Width must be greater than 0");
+      //}
+       // } 
+       // get area(){
+         //   return this._length * this._width;
+       // }   
+    // }
+    // const rectangle = new Rectangle(60,35);
+    // console.log(rectangle.area);
+
+
+
+
